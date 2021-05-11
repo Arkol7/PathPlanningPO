0459455996a727df239749db23046bf7c7569114
Arkol7 <ar.ko.latyshev@gmail.com>
2021-05-11 19:26:46 +0300

    clear unnecessary files and reformat utils.py

--- common/utils.py
+++ common/utils.py
@@ -2,6 +2,7 @@ from functools import wraps
 from timeit import default_timer as timer
 import math
 
+
 def timed(f):
     @wraps(f)
     def wrap(*args, **kw):
@@ -9,14 +10,17 @@ def timed(f):
         result = f(*args, **kw)
         elapsed = timer() - start
         return result, elapsed
+
     return wrap
+
 
 def ManhattanDistance(i1, j1, i2, j2):
     return abs(int(i1) - int(i2)) + abs(int(j1) - int(j2))
 
 
 def DiagonalDistance(i1, j1, i2, j2):
-    return abs(abs(i1-i2)-abs(j1-j2)) + math.sqrt(2)*min(abs(i1-i2),abs(j1-j2))
+    return abs(abs(i1 - i2) - abs(j1 - j2)) + math.sqrt(2) * min(abs(i1 - i2), abs(j1 - j2))
+
 
 def ChebyshevDistance(i1, j1, i2, j2):
     return max([abs(int(i1) - int(i2)), abs(int(j1) - int(j2))])
